ee4dc3aaab965589ebbd5ed055efd9fc5717e55d
Ana Fretes <anafretes@proton.me>
2024-10-04 16:26:03 -0400

    First commit

---
+++ src/PairProgramming/Main.java
@@ -0,0 +1,14 @@
+package PairProgramming;
+
+public class Main {
+    public static void main(String[] args) {
+//        Cat cat1 = new Cat("Microondas","Nose");
+//        Cat cat2 = new Cat("Microondas2","Nose");
+//        Dog dog1 = new Dog("Rompe","Delmer");
+//        dog1.makeSound();
+//        System.out.println(Animal.totalAnimals);
+//        System.out.println(Dog.totalDogs);
+//        System.out.println(Cat.totalCats);
+//        cat1.makeSound();
+    }
+}
